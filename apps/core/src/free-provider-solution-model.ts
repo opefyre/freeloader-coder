cded6e9fe12e3dd0bf543d2d06ef49154bb5d27c
Opefyre <opefyre@gmail.com>
2026-08-20 10:46:40 +0100

    fix: invalidate stale specialist routing evidence

--- apps/core/src/free-provider-solution-model.ts
+++ apps/core/src/free-provider-solution-model.ts
@@ -9,7 +9,7 @@ import { projectEgressPermitSchema, researchEvidenceGraphSchema, solutionContent
 import type { RoutedSolutionModel, SolutionModelEvidence } from "./project-solution-orchestrator.js";
 
 const SENSITIVE = /(?:api[_-]?key|password|private[_-]?key|access[_-]?token|secret)["']?\s*[:=]|-----BEGIN [A-Z ]*PRIVATE KEY-----|\/Users\/[^/\s]+\/|[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}|\+\d[\d ()-]{8,}\d/i;
-const RESPONSE_CONTRACT_VERSION = 2;
+const RESPONSE_CONTRACT_VERSION = 3;
 
 export class FreeProviderSolutionModel implements RoutedSolutionModel {
   readonly #runtime: ProviderRuntimeService;
